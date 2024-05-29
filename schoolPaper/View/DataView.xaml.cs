6c50829773e77e063e193624aba93f49d44fb507
thinkpad <thinkpad@LAPTOP-LFT90UUT>
2024-05-29 19:04:27 +0200

    Add project files.

---
+++ schoolPaper/View/DataView.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ConsoleApp1.Data;
+
+namespace schoolPaper.View
+{
+    /// <summary>
+    /// Interaction logic for DataView.xaml
+    /// </summary>
+    public partial class DataView : UserControl
+    {
+        private readonly VContent _context;
+        private Emberek _selectedUser;
+
+        public DataView()
+        {
+            InitializeComponent();
+            _context = new VContent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var users = _context.Emberek.ToList();
+            listBox.ItemsSource = users;
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox.SelectedIndex != -1)
+            {
+                _selectedUser = (Emberek)listBox.SelectedItem;
+                nevTextbox.Text = _selectedUser.Nev;
+                korTextbox.Text = _selectedUser.Kor;
+                varosTextbox.Text = _selectedUser.Varos;
+                meloTextbox.Text = _selectedUser.Melo;
+                hobbiTextbox.Text = _selectedUser.Hobbi;
+            }
+        }
+
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedUser != null)
+            {
+                _selectedUser.Nev = nevTextbox.Text;
+                _selectedUser.Kor = korTextbox.Text;
+                _selectedUser.Varos = varosTextbox.Text;
+                _selectedUser.Melo = meloTextbox.Text;
+                _selectedUser.Hobbi = hobbiTextbox.Text;
+
+                _context.Emberek.Update(_selectedUser);
+                await _context.SaveChangesAsync();
+                LoadData();
+            }
+        }
+
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedUser != null)
+            {
+                _context.Emberek.Remove(_selectedUser);
+                await _context.SaveChangesAsync();
+                LoadData();
+                ClearFields();
+            }
+        }
+
+        private void ClearFields()
+        {
+            nevTextbox.Text = "";
+            korTextbox.Text = "";
+            varosTextbox.Text = "";
+            meloTextbox.Text = "";
+            hobbiTextbox.Text = "";
+        }
+
+        private void ageTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ schoolPaper/ViewModel/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using schoolPaper.Command;
+using schoolPaper.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schoolPaper.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+		private object currentView;
+
+		public object CurrentView
+		{
+			get { return currentView; }
+			set { currentView = value; OnPropertyChanged(); }
+		}
+
+        HomeView homeView;
+        TaskOneView taskOneView;
+        DataView dataView;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public RelayCommand OpenHomeCommand { get; }
+        public RelayCommand OpenTaskOneViewCommand { get; }
+
+        public MainViewModel()
+        {
+            homeView = new HomeView();
+            taskOneView = new TaskOneView();
+            dataView = new DataView();
+
+            OpenHomeCommand = new RelayCommand(OpenHome);
+            OpenTaskOneViewCommand = new RelayCommand(OpenTaskOneView);
+
+            CurrentView = homeView;
+        }
+
+        private void OpenHome(object parameter)
+        {
+            CurrentView = homeView;
+        }
+
+        private void OpenTaskOneView(object parameter)
+        {
+            CurrentView = taskOneView;
+        }        
+        
+        private void DataView(object parameter)
+        {
+            DataView = dataView;
+        }
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
